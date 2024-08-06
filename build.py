2635e7300bc3e639b50e3e32640816d981fae44e
Michele Dolfi <dol@zurich.ibm.com>
2024-08-06 12:37:25 +0200

    make build.py raise on errors
    Signed-off-by: Michele Dolfi <dol@zurich.ibm.com>

--- build.py
+++ build.py
@@ -1,52 +1,50 @@
 #!/usr/bin/env python
 
 import os
-import re
 
 import sys
 
-import json
-import glob
-
-import argparse
 import subprocess
 
-#from deepsearch_glm.utils.load_pretrained_models import load_pretrained_nlp_models
+ROOT_DIR = os.path.abspath("./")
+BUILD_DIR = os.path.join(ROOT_DIR, "build")
 
-ROOT_DIR=os.path.abspath("./")
-BUILD_DIR=os.path.join(ROOT_DIR, "build")
 
 def run(cmd, cwd="./"):
 
     print(f"\nlaunch: {cmd}")
-    
+
     parts = cmd.split(" ")
-    message = subprocess.run(parts, cwd=cwd)    
+    message = subprocess.run(parts, cwd=cwd)
 
     if "returncode=0" in str(message):
         print(f" -> SUCCESS")
         return True
 
-    print(f" -> ERROR with message: '{message}'\n")        
+    print(f" -> ERROR with message: '{message}'\n")
     return False
-    
+
+
 def build_local(multi_threaded=True):
-    
+
     if not os.path.exists(BUILD_DIR):
         print("python executable: ", sys.executable)
-        
+
         cmd = f"cmake -B {BUILD_DIR} -DPYTHON_EXECUTABLE={sys.executable}"
-        run(cmd, cwd=ROOT_DIR)
+        success = run(cmd, cwd=ROOT_DIR)
+        if not success:
+            raise RuntimeError("Error building.")
     else:
         print(f"build directory detected: {BUILD_DIR}")
 
+    cmd = f"cmake --build {BUILD_DIR} --target install"
     if multi_threaded:
-        cmd = f"cmake --build {BUILD_DIR} --target install -j"    
-        run(cmd, cwd=ROOT_DIR)    
-    else:
-        cmd = f"cmake --build {BUILD_DIR} --target install"
-        run(cmd, cwd=ROOT_DIR)    
+        cmd += " -j"
+    success = run(cmd, cwd=ROOT_DIR)
+    if not success:
+        raise RuntimeError("Error building.")
 
-if "__main__"==__name__:
+
+if "__main__" == __name__:
 
     build_local(multi_threaded=False)
