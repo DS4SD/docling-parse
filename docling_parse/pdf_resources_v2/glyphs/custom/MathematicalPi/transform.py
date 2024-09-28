edb908369ac9f4df0369a45bcf84aeefaa802542
Peter Staar <taa@zurich.ibm.com>
2024-09-28 09:29:33 +0200

    reformatted the python-code
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- docling_parse/pdf_resources_v2/glyphs/custom/MathematicalPi/transform.py
+++ docling_parse/pdf_resources_v2/glyphs/custom/MathematicalPi/transform.py
@@ -1,9 +1,8 @@
-
-data_uni=[]
-data_hex=[]
+data_uni = []
+data_hex = []
 
 with open("MathematicalPi.dat.orig", "r") as fd:
-    
+
     lines = fd.readlines()
 
     for line in lines:
@@ -15,14 +14,14 @@ with open("MathematicalPi.dat.orig", "r") as fd:
         try:
             dec = int(parts[1].replace(" ", ""), 16)
             data_uni.append([parts[0], chr(dec), val])
-            
+
         except:
             data_hex.append([parts[0], parts[1]])
-     
-with open("MathematicalPi.hex.dat", "w") as fd:            
-    for row in data_hex:
-        fd.write(";".join(row)+"\n")
 
-with open("MathematicalPi.uni.dat", "w") as fd:            
+with open("MathematicalPi.hex.dat", "w") as fd:
+    for row in data_hex:
+        fd.write(";".join(row) + "\n")
+
+with open("MathematicalPi.uni.dat", "w") as fd:
     for row in data_uni:
-        fd.write(";".join(row)+"\n")
+        fd.write(";".join(row) + "\n")
