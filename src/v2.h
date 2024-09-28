7cf9fdd97d659416815bf1106b7a54f00468a0d5
Peter Staar <taa@zurich.ibm.com>
2024-09-28 06:00:50 +0200

    refactoring the v2
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>
    Signed-off-by: Christoph Auer <cau@zurich.ibm.com>

--- src/v2.h
+++ src/v2.h
@@ -1,59 +1,57 @@
 //-*-C++-*-
 
-#include "pdf/enums.h"
-#include "pdf/utils.h"
+#include <v2/enums.h>
+#include <v2/utils.h>
 
-#include "pdf/qpdf/to_json.h"
-#include "pdf/qpdf/instruction.h"
-#include "pdf/qpdf/stream_decoder.h"
+#include <v2/qpdf/to_json.h>
+#include <v2/qpdf/instruction.h>
+#include <v2/qpdf/stream_decoder.h>
 
-#include "pdf/pdf_resource.h"
-#include "pdf/pdf_resources/page_dimension.h"
+#include <v2/pdf_resource.h>
+#include <v2/pdf_resources/page_dimension.h>
 
-#include "pdf/pdf_resources/page_font/glyphs.h"
-#include "pdf/pdf_resources/page_font/font_cid.h"
-#include "pdf/pdf_resources/page_font/font_cids.h"
-#include "pdf/pdf_resources/page_font/encoding.h"
-#include "pdf/pdf_resources/page_font/encodings.h"
-#include "pdf/pdf_resources/page_font/base_font.h"
-#include "pdf/pdf_resources/page_font/base_fonts.h"
-#include "pdf/pdf_resources/page_font/cmap.h"
-#include "pdf/pdf_resources/page_font/char_description.h"
-#include "pdf/pdf_resources/page_font/char_processor.h"
+#include <v2/pdf_resources/page_font/glyphs.h>
+#include <v2/pdf_resources/page_font/font_cid.h>
+#include <v2/pdf_resources/page_font/font_cids.h>
+#include <v2/pdf_resources/page_font/encoding.h>
+#include <v2/pdf_resources/page_font/encodings.h>
+#include <v2/pdf_resources/page_font/base_font.h>
+#include <v2/pdf_resources/page_font/base_fonts.h>
+#include <v2/pdf_resources/page_font/cmap.h>
+#include <v2/pdf_resources/page_font/char_description.h>
+#include <v2/pdf_resources/page_font/char_processor.h>
 
-#include "pdf/pdf_resources/page_font.h"
-#include "pdf/pdf_resources/page_fonts.h"
+#include <v2/pdf_resources/page_font.h>
+#include <v2/pdf_resources/page_fonts.h>
 
-#include "pdf/pdf_resources/page_grph.h"
-#include "pdf/pdf_resources/page_grphs.h"
+#include <v2/pdf_resources/page_grph.h>
+#include <v2/pdf_resources/page_grphs.h>
 
-#include "pdf/pdf_resources/page_xobject.h"
-#include "pdf/pdf_resources/page_xobjects.h"
+#include <v2/pdf_resources/page_xobject.h>
+#include <v2/pdf_resources/page_xobjects.h>
 
-#include "pdf/pdf_resources/page_cell.h"
-#include "pdf/pdf_resources/page_cells.h"
+#include <v2/pdf_resources/page_cell.h>
+#include <v2/pdf_resources/page_cells.h>
 
-#include "pdf/pdf_resources/page_line.h"
-#include "pdf/pdf_resources/page_lines.h"
+#include <v2/pdf_resources/page_line.h>
+#include <v2/pdf_resources/page_lines.h>
 
-#include "pdf/pdf_resources/page_image.h"
-#include "pdf/pdf_resources/page_images.h"
+#include <v2/pdf_resources/page_image.h>
+#include <v2/pdf_resources/page_images.h>
 
-#include "pdf/pdf_sanitator.h"
-#include "pdf/pdf_sanitators/lines.h"
-#include "pdf/pdf_sanitators/cells.h"
-#include "pdf/pdf_sanitators/page_dimension.h"
+#include <v2/pdf_sanitator.h>
+#include <v2/pdf_sanitators/lines.h>
+#include <v2/pdf_sanitators/cells.h>
+#include <v2/pdf_sanitators/page_dimension.h>
 
-#include "pdf/pdf_state.h"
-#include "pdf/pdf_states/text.h"
-#include "pdf/pdf_states/line.h"
-#include "pdf/pdf_states/grph.h"
-#include "pdf/pdf_states/global.h"
+#include <v2/pdf_state.h>
+#include <v2/pdf_states/text.h>
+#include <v2/pdf_states/line.h>
+#include <v2/pdf_states/grph.h>
+#include <v2/pdf_states/global.h>
 
-#include "pdf/pdf_decoder.h"
-#include "pdf/pdf_decoders/stream_enums.h"
-#include "pdf/pdf_decoders/stream.h"
-#include "pdf/pdf_decoders/page.h"
-#include "pdf/pdf_decoders/document.h"
-
-//#include "pdf/parser.h"
+#include <v2/pdf_decoder.h>
+#include <v2/pdf_decoders/stream_enums.h>
+#include <v2/pdf_decoders/stream.h>
+#include <v2/pdf_decoders/page.h>
+#include <v2/pdf_decoders/document.h>
