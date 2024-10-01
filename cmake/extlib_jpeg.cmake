a97ace45d10b43213a6bd556e6ffd88d4980cf2f
Michele Dolfi <dol@zurich.ibm.com>
2024-10-01 11:43:14 +0200

    feat: introduce USE_SYSTEM_DEPS for building with system packages
    Signed-off-by: Michele Dolfi <dol@zurich.ibm.com>

--- cmake/extlib_jpeg.cmake
+++ cmake/extlib_jpeg.cmake
@@ -1,36 +1,54 @@
 
 message(STATUS "entering in extlib_jpeg.cmake")
 
-include(ExternalProject)
-include(CMakeParseArguments)
+set(ext_name "jpeg")
 
-set(JPEG_URL https://github.com/libjpeg-turbo/libjpeg-turbo.git)
-set(JPEG_TAG 3.0.3)
+if(USE_SYSTEM_DEPS)
+    find_package(PkgConfig)
+    pkg_check_modules(libjpeg REQUIRED libjpeg)
 
-ExternalProject_Add(extlib_jpeg
-    PREFIX extlib_jpeg
+    add_library(${ext_name} STATIC IMPORTED)
+    set_target_properties(${ext_name} PROPERTIES INTERFACE_LINK_LIBRARIES "${libjpeg_LIBRARIES}")
+    set_target_properties(${ext_name} PROPERTIES INTERFACE_LINK_DIRECTORIES "${libjpeg_LIBRARY_DIRS}")
+    set_target_properties(${ext_name} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${libjpeg_INCLUDEDIR}")
 
-    UPDATE_COMMAND ""
-    GIT_REPOSITORY ${JPEG_URL}
-    GIT_TAG ${JPEG_TAG}
+else()
+    include(ExternalProject)
+    include(CMakeParseArguments)
 
-    BUILD_ALWAYS OFF
+    set(JPEG_URL https://github.com/libjpeg-turbo/libjpeg-turbo.git)
+    set(JPEG_TAG 3.0.3)
 
-    INSTALL_DIR ${EXTERNALS_PREFIX_PATH}
+    ExternalProject_Add(extlib_jpeg
 
-    CMAKE_ARGS \\
-    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \\
-    -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES} \\
-    -DCMAKE_C_FLAGS=${ENV_ARCHFLAGS} \\
-    -DARMV8_BUILD=ON \\
-    -DCMAKE_INSTALL_LIBDIR=${EXTERNALS_PREFIX_PATH}/lib \\
-    -DCMAKE_INSTALL_PREFIX=${EXTERNALS_PREFIX_PATH}
+        PREFIX extlib_jpeg
 
-    BUILD_IN_SOURCE ON
-    LOG_DOWNLOAD ON
-    # LOG_BUILD ON
-)
+        UPDATE_COMMAND ""
+        GIT_REPOSITORY ${JPEG_URL}
+        GIT_TAG ${JPEG_TAG}
 
-add_library(jpeg STATIC IMPORTED)
-set_target_properties(jpeg PROPERTIES IMPORTED_LOCATION ${EXTERNALS_PREFIX_PATH}/lib/libjpeg.a)
-add_dependencies(jpeg extlib_jpeg)
+        BUILD_ALWAYS OFF
+
+        INSTALL_DIR ${EXTERNALS_PREFIX_PATH}
+
+        CMAKE_ARGS \\
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON \\
+        -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES} \\
+        -DCMAKE_C_FLAGS=${ENV_ARCHFLAGS} \\
+        -DARMV8_BUILD=ON \\
+        -DCMAKE_INSTALL_LIBDIR=${EXTERNALS_PREFIX_PATH}/lib \\
+        -DCMAKE_INSTALL_PREFIX=${EXTERNALS_PREFIX_PATH}
+
+        BUILD_IN_SOURCE ON
+        LOG_DOWNLOAD ON
+        # LOG_BUILD ON
+    )
+
+    add_library(${ext_name} STATIC IMPORTED)
+    add_dependencies(${ext_name} extlib_jpeg)
+    set_target_properties(${ext_name} PROPERTIES
+        IMPORTED_LOCATION ${EXTERNALS_PREFIX_PATH}/lib/libjpeg.a
+        INTERFACE_INCLUDE_DIRECTORIES ${EXTERNALS_PREFIX_PATH}/include
+    )
+
+endif()
