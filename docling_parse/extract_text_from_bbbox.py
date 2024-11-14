65d1cb1295f2fb706bbc798b2ea2597d2d787e12
Peter Staar <taa@zurich.ibm.com>
2024-11-14 09:59:28 +0100

    reformatted the code
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- docling_parse/extract_text_from_bbbox.py
+++ docling_parse/extract_text_from_bbbox.py
@@ -7,6 +7,7 @@ from tabulate import tabulate
 
 from docling_parse import pdf_parser_v2
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Process a PDF file.")
 
@@ -41,7 +42,7 @@ def parse_args():
 
     # Check if the PDF file exists
     assert os.path.exists(args.input_pdf), f"PDF file does not exist: {args.input_pdf}"
-    
+
     return (
         args.log_level,
         args.input_pdf,
@@ -78,10 +79,14 @@ def main():
     parser.unload_document(doc_key)
 
     selected_cells = doc["pages"][0]["original"]["cells"]["data"][10:20]
-    print(tabulate(selected_cells, headers=doc["pages"][0]["original"]["cells"]["header"]))
+    print(
+        tabulate(selected_cells, headers=doc["pages"][0]["original"]["cells"]["header"])
+    )
 
-    parser.sanitize_cells(selected_cells, )
-    
-    
+    parser.sanitize_cells(
+        selected_cells,
+    )
+
+
 if __name__ == "__main__":
     main()
